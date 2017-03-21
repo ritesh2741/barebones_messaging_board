705b5531f15079f6656fc4982e794312efcd8872
Ritesh Shrestha <riteshshr1992@gmail.com>
2017-03-21 02:27:04 -0400

    Added names for user in registration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321041910) do
+ActiveRecord::Schema.define(version: 20170321061024) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20170321041910) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.text     "about"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
