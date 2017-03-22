95c79fd644b90500040a06e31d04f7c54827faa6
Ritesh Shrestha <riteshshr1992@gmail.com>
2017-03-21 23:10:22 -0400

    Added User info to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
-  validates :title, presence: true, length: {minimum: 5}
+  validates :title, presence: true
   validates :body, presence: true
 end
